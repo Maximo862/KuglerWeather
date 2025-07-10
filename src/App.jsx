8f0b757df2dc7dbccd3d6b612c8fedbf51165c0f
Maximo <Maximokugler9@gmail.com>
2025-07-10 19:54:21 -0300

    Add centerpanel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { CenterPanel } from "./Components/CenterPanel";
 
-export function App() {
- 
-
+function App() {
   return (
-    <>
-     
-    </>
-  )
+    <div className="container-fluid p-4" style={{ minHeight: "100vh" }}>
+      <div className="row g-4 h-100">
+        <CenterPanel />
+      </div>
+    </div>
+  );
 }
 
+export default App;
